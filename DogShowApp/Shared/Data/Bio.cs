4efcea1d36d48a183999924319892c05736788a9
MadMalcolm <malcolmkhummel3@gmail.com>
2022-10-18 13:03:26 -0400

    Added Bio Class...working on implementating CreateBio and FetchBio

---
+++ DogShowApp/Shared/Data/Bio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogShowApp.Shared.Data
+{
+    internal class Bio
+    {
+        public string? DogName { get; set; }
+        public string? DogDescription { get; set; }
+        public string? TrainerName { get; set; }
+        public string? DogImageURL { get; set; }
+        public string? TrainerImageURL { get; set; }
+
+        public Bio(string? dogName, string? dogDescription, string? trainerName, string? dogImageURL, string? trainerImageURL)
+        {
+            this.DogName = dogName;
+            this.DogDescription = dogDescription;
+            this.TrainerName = trainerName;
+            this.DogImageURL = dogImageURL;
+            this.TrainerImageURL = trainerImageURL;
+        }
+
+        public Bio() { }
+    }
+}
