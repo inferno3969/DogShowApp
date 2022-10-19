5cd75a4e18d382e2194fa02fb8788abc4496293d
Nathanael Allison <howlingflame39@gmail.com>
2022-10-19 18:22:12 -0400

    working checkout system

--- DogShowApp/Shared/Data/Ticket.cs
+++ DogShowApp/Shared/Data/Ticket.cs
@@ -5,17 +5,14 @@ namespace DogShowApp.Shared.Data
 {
     public class Ticket
     {
-        public string regularTicket { get; set; } = "regular ticket";
-
-        public string seasonTicket { get; set; } = "season ticket";
 
         [MinLength(0, ErrorMessage = "You can't have negative amount of tickets.")]
-        public int numOfRegularTickets { get; set; }
+        public int numOfEventTickets { get; set; }
 
         [MinLength(0, ErrorMessage = "You can't have negative amount of tickets.")]
         public int numOfSeasonTickets { get; set; }
 
-        public decimal regularTicketsPrice { get; set; } = 10.00m;
+        public decimal eventTicketsPrice { get; set; } = 10.00m;
 
         public decimal seasonTicketsPrice { get; set; } = 100.00m;
 
