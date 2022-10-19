5a45d37e5c9c3ea18afbec71c68bb2b4ff47df99
MadMalcolm <malcolmkhummel3@gmail.com>
2022-10-19 18:21:01 -0400

    finally got some ability to control image size and text size...will be able to mess with css....committing before merging with nates branch...need the structure to be the same to add a page

--- DogShowApp/Server/Controllers/BioController.cs
+++ DogShowApp/Server/Controllers/BioController.cs
@@ -26,8 +26,13 @@ namespace DogShowApp.Server.Controllers
 
             if (Bios.Count == 0)
             {
-                Bios.Add(new Bio("Sally", "Struthers", "Brutus", "https://www.quotationof.com/images/eric-wareheim-6.jpg", "https://www.quotationof.com/images/eric-wareheim-6.jpg"));
-               // Bios.Add(new Bio("Cleetus McPaw", "German Shiza", "Rammstein von McBraun", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F564x%2Fc6%2F60%2Ff9%2Fc660f9b3be0dba367ea009e192dbd24d.jpg&f=1&nofb=1&ipt=78421f4096cf7aefb6b2f0c474034a79e0000cde55070d10a141433114f2a607&ipo=images", "img/IMG_4018.jpg"));
+                Bios.Add(new Bio("Sally", "Hungarian Blue Ball", "Brutus", "https://i0.wp.com/voiceofoc.org/wp-content/uploads/archive/editorial/5/2d/52dea990-2085-11e3-8b31-0019bb2963f4/5239ded32a185.image.jpg?w=460&ssl=1", "A wonderful very playful and well trained Hungarian Blue Ball. "));
+                Bios.Add(new Bio("Cleetus McPaw", "German Shiza", "Rammstein von McBraun", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F564x%2Fc6%2F60%2Ff9%2Fc660f9b3be0dba367ea009e192dbd24d.jpg&f=1&nofb=1&ipt=78421f4096cf7aefb6b2f0c474034a79e0000cde55070d10a141433114f2a607&ipo=images", "Best Tricks include tear hands and tear face. "));
+                Bios.Add(new Bio("Dwayne", "Dual Head German Shiza", "Bo Peep", "https://www.aidedd.org/dnd/images/death-dog.jpg", "This guy has double teeth for double violence"));
+                Bios.Add(new Bio("Alvarez", "Little Shitzu", "Robert Kraft", "https://i.guim.co.uk/img/media/a10a67810303b0389ef6c75b2468156448377f16/0_30_6000_3600/master/6000.jpg?width=620&quality=85&dpr=1&s=none", "Don't judge a book by it's cover. This guy can consume flesh pound for pound at a higher rate than any other breed"));
+
+
+
             }
         }
 
