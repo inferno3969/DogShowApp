fbdbb9fc76fa7994125c50dbdeb50be8d8e96069
MadMalcolm <malcolmkhummel3@gmail.com>
2022-10-20 08:31:20 -0400

    going to change bio controller to access Bios directly for put..hopefully works better

--- DogShowApp/Server/Controllers/BioController.cs
+++ DogShowApp/Server/Controllers/BioController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using DogShowApp.Shared.Data;
 using System.Globalization;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.Text.Json;
 
 namespace DogShowApp.Server.Controllers
 {
@@ -27,9 +29,9 @@ namespace DogShowApp.Server.Controllers
 
             if (Bios.Count == 0)
             {
-                Bios.Add(new Bio("Sally", "Hungarian Blue Ball", "Brutus", "https://i0.wp.com/voiceofoc.org/wp-content/uploads/archive/editorial/5/2d/52dea990-2085-11e3-8b31-0019bb2963f4/5239ded32a185.image.jpg?w=460&ssl=1", "A wonderful very playful and well trained Hungarian Blue Ball. "));
+                Bios.Add(new Bio("Brutus", "Hungarian Blue Ball", "Billy Jo Armstrong", "https://i0.wp.com/voiceofoc.org/wp-content/uploads/archive/editorial/5/2d/52dea990-2085-11e3-8b31-0019bb2963f4/5239ded32a185.image.jpg?w=460&ssl=1", "A wonderful very playful and well trained Hungarian Blue Ball. "));
                 Bios.Add(new Bio("Cleetus McPaw", "German Shiza", "Rammstein von McBraun", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F564x%2Fc6%2F60%2Ff9%2Fc660f9b3be0dba367ea009e192dbd24d.jpg&f=1&nofb=1&ipt=78421f4096cf7aefb6b2f0c474034a79e0000cde55070d10a141433114f2a607&ipo=images", "Best Tricks include tear hands and tear face. "));
-                Bios.Add(new Bio("Dwayne", "Dual Head German Shiza", "Bo Peep", "https://www.aidedd.org/dnd/images/death-dog.jpg", "This guy has double teeth for double violence"));
+                Bios.Add(new Bio("Dwayne", "Dual Headed Long Hair German Shiza", "Till Lunderson", "https://www.aidedd.org/dnd/images/death-dog.jpg", "This guy has double teeth for double violence"));
                 Bios.Add(new Bio("Alvarez", "Little Shitzu", "Robert Kraft", "https://i.guim.co.uk/img/media/a10a67810303b0389ef6c75b2468156448377f16/0_30_6000_3600/master/6000.jpg?width=620&quality=85&dpr=1&s=none", "Don't judge a book by it's cover. This guy can consume flesh pound for pound at a higher rate than any other breed"));
 
 
@@ -50,10 +52,32 @@ namespace DogShowApp.Server.Controllers
         }
 
         [HttpPut]
-        public void Put(Tuple<Int32, Event> putStuff)
-        {
-            Bios.RemoveAt(putStuff.Item1);
-            Bios.Insert(putStuff.Item1, putStuff.Item2);
+        public void Put(Tuple<Int32, List<Object?>> properties)
+        { 
+            Bio newBio = new Bio();
+            PropertyInfo[] prop = newBio.GetType().GetProperties();
+
+
+            for (int i = 0; i < properties.Item2.Count; i++)
+            {
+                if (properties.Item2[i] != null && properties.Item2[i].GetType() == typeof(JsonElement))
+                {
+                    if (prop[i].PropertyType == typeof(String))
+                    {
+                        prop[i].SetValue(newBio, properties.Item2[i].ToString());
+                    }
+                    else if (prop[i].PropertyType == typeof(Boolean))
+                    {
+                        prop[i].SetValue(newBio, Convert.ToBoolean(properties.Item2[i].ToString()));
+                    }
+                    else if (prop[i].PropertyType == typeof(Int32))
+                    {
+                        prop[i].SetValue(newBio, Convert.ToInt32(properties.Item2[i].ToString()));
+                    }
+                }
+            }
+            Bios.RemoveAt(properties.Item1);
+            Bios.Insert(properties.Item1, newBio);
         }
         [HttpDelete]
         public void Delete(String item)
